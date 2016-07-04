4c69bb8c555a7e58a225a7505d0e56e24fb25b91
laznrbfe <laznrbfe@163.com>
2016-07-04 20:55:32 +0800

    init

---
+++ JSPatchFrameworkSwift/JSPatchFrameworkSwiftTests/JSPatchFrameworkSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  JSPatchFrameworkSwiftTests.swift
+//  JSPatchFrameworkSwiftTests
+//
+//  Created by 张青明 on 16/7/4.
+//  Copyright © 2016年 极客栈. All rights reserved.
+//
+
+import XCTest
+@testable import JSPatchFrameworkSwift
+
+class JSPatchFrameworkSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
